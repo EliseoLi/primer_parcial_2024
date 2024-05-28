f4151b70c7fa2e58fe5d6450dcacaece477ff6af
EliseoLi <eliseolillo05@gmail.com>
2024-05-27 21:27:17 -0300

    Segundo Commit de Primer Parcial
    Correcion al momento de crear la memoria dinamica de tipo uint64_t y correccion de llamado de la funcion en el for extractHeader (1 vez solamente)

--- main.cpp
+++ main.cpp
@@ -10,27 +10,20 @@ struct Header
     uint64_t lower_level_devices_count:16;
     uint64_t ID:16;
 };
-/*
-uint64_t extract64(uint64_t data, uint64_t inicio, uint64_t final)
-{
-    uint64_t num_bits = final - inicio + 1; // Número de bits a extraer
-    uint64_t mask = (1 << num_bits) - 1; // Máscara para extraer los bits
-    return (data >> inicio) & mask; // Desplazar a la derecha y aplicar máscara
-}
-*/
+//Funcion para extraer bit de 64 bits 
 uint64_t extract64(uint64_t data, uint32_t inicio, uint32_t final) {
     uint32_t num_bits = final - inicio + 1; // Número de bits a extraer
     uint64_t mask = (1ULL << num_bits) - 1; // Máscara para extraer los bits
     return (data >> inicio) & mask; // Desplazar a la derecha y aplicar máscara
 }
-struct Header extract64(uint64_t encabezadoData)
-{
+//Funcion para extraer cada bits del header
+Header extractHeader(uint64_t encabezadoData) {
     Header head;
-    head.upper_level_device_id=extract64(encabezadoData,0,15);
-    head.info=extract64(encabezadoData,16,23);
+    head.upper_level_device_id = extract64(encabezadoData, 0, 15);
+    head.info = extract64(encabezadoData, 16, 23);
     head.device_type = extract64(encabezadoData, 24, 31);
-    head.lower_level_devices_count=extract64(encabezadoData,32,47);
-    head.ID=extract64(encabezadoData,48,63);
+    head.lower_level_devices_count = extract64(encabezadoData, 32, 47);
+    head.ID = extract64(encabezadoData, 48, 63);
     return head;
 }
 
@@ -42,8 +35,9 @@ int main()
     cant=(ftell(f)/sizeof(struct Header)); //Cantidad de datos
     printf("\n\nCantidad de paquetes:%d\n\n",cant);
     fseek(f,0,SEEK_SET);
-    //Cantidad de paquetes es 24
-    struct Header *data=new struct Header[cant];
+    //Cantidad de paquetes es 12
+    
+    uint64_t *data = new uint64_t[cant];
     int result = fread(data, sizeof(Header), cant, f);
     if (result != cant) 
     {
@@ -55,14 +49,13 @@ int main()
 
     for (int i = 0; i < cant; i++)
     {
-        struct Header head = extract64(data[i].device_type);
-        struct Header head_1 = extract64(data[i].ID);
-        struct Header head_2 = extract64(data[i].lower_level_devices_count);
-        struct Header head_3 = extract64(data[i].upper_level_device_id);
-        printf("ID:%u \n",head_1.ID);
-        printf("Cantidad de equipos conectados:%u\n",head_2.lower_level_devices_count);
-        printf("ID Del equipo conectado:%u\n",head_3.upper_level_device_id);
-       if(head.device_type==0)
+        //Repensamos como trabajamos un uint64, llamamos a la memoria dinamica de tipo uint64_t
+        struct Header head = extractHeader(data[i]);
+        printf("--------------\n");
+        printf("ID: %u \n",head.ID);
+        printf("Cantidad de equipos conectados:%u\n",head.lower_level_devices_count);
+        printf("ID Del equipo conectado:%u\n",head.upper_level_device_id);
+        if(head.device_type==0)
         {
             //head.device_type
             printf("Registro de Equipo :CPU\n");
@@ -103,6 +96,6 @@ int main()
         {
             printf("Registro de equipo:CONCENTRADOR\n");
         } 
+    delete[] data;
     }
 }
-
